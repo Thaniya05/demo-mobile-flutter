4c21604cf490b0653e89f2860958db5e064f6247
thaniya <thaniya14@gmail.com>
2023-07-24 16:46:28 +0700

    update Intopage , Login appbar

--- demomobile/lib/pages/LoginPage.dart
+++ demomobile/lib/pages/LoginPage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,12 +7,35 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [],
+      appBar: AppBar(
+        //excludeHeaderSemantics: true,
+        toolbarHeight: 200,
+        flexibleSpace: Container(
+          //color: Colors.amber,
+          child: CarouselSlider(
+            items: [1, 2, 3, 4]
+                .map(
+                  (e) => Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(child: Text("${e}")),
+                    color: Colors.blueGrey[e * 100],
+                  ),
+                )
+                .toList(),
+            options: CarouselOptions(
+              aspectRatio: 16 / 9,
+              height: 200,
+              //enlargeCenterPage: true,
+              viewportFraction: 1,
+              autoPlay: true,
+            ),
+          ),
+        ),
+        //title: Text("Hello"),
       ),
+      body: Container(
+          // color: //Colors.amber,
+          ),
     );
   }
 }
--- demomobile/lib/route.dart
+++ demomobile/lib/route.dart
@@ -1,9 +1,19 @@
 import 'package:demomobile/pages/HomePage.dart';
+import 'package:demomobile/pages/IntroPage.dart';
+import 'package:demomobile/pages/LoginPage.dart';
 import 'package:get/get.dart';
 
 List<GetPage> myroute = [
   GetPage(
     name: "/",
     page: () => HomePage(),
+  ),
+  GetPage(
+    name: "/intro",
+    page: () => IntroPage(),
+  ),
+  GetPage(
+    name: "/login",
+    page: () => LoginPage(),
   ),
 ];
